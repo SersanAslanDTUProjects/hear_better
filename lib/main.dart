7af0012db55e3d1ebd9926c7c661b77aabe91334
Helle <helle1926@hotmail.com>
2020-10-01 18:17:58 +0200

    pop up window
    some features for pop up window (minimal usage)

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hear_better/screens/dialogs/test_connection.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
       title: 'HearBetter',
       theme: ThemeData(
       ),
-      home: MyHomePage(title: 'HearBetter'),
+      home: testConnection(),
     );
   }
 }
---
+++ lib/screens/dialogs/test_connection.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class testConnection extends StatefulWidget {
+  @override
+  _testConnectionState createState() => _testConnectionState();
+}
+
+class _testConnectionState extends State<testConnection> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+      floatingActionButton: FloatingActionButton(
+        child: Text("click me!"),
+        onPressed: () {
+          showDialog(
+            context: context,
+            builder: (BuildContext context) => _earTestPopUp(context),
+          );
+        },
+        backgroundColor: Colors.grey,
+      ),
+    ));
+  }
+
+  Widget _earTestPopUp(BuildContext context) {
+    return new AlertDialog(
+      title: Text("About pop up"),
+      content: new Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Image.asset('assets/rightEar.png'),
+        ],
+      ),
+      actions: <Widget>[
+        new FlatButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            child: Image.asset('assets/no.png')),
+        new FlatButton(
+          onPressed: () {
+
+          },
+          child: Image.asset('assets/yes.png'),
+        ),
+      ],
+    );
+  }
+}
