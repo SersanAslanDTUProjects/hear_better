84c1a271bcad2f2b7211c79ac8a4ad57a3b41f41
Helle <helle1926@hotmail.com>
2020-10-04 00:05:43 +0200

    added text

--- lib/screens/dialogs/test_connection.dart
+++ lib/screens/dialogs/test_connection.dart
@@ -11,9 +11,11 @@ class _TestConnectionState extends State<TestConnection> {
     return MaterialApp(
         home: Scaffold(
       floatingActionButton: FloatingActionButton(
-        child: Text("click me!"),
+        child: Text("click me!",
+          textAlign: TextAlign.center,),
         onPressed: () {
           showDialog(
+            barrierDismissible: false,
             context: context,
             builder: (BuildContext context) => _earTestPopUp(context),
           );
@@ -25,26 +27,57 @@ class _TestConnectionState extends State<TestConnection> {
 
   Widget _earTestPopUp(BuildContext context) {
     return new AlertDialog(
-      title: Text("About pop up"),
-      content: new Column(
+      title: Text("Testing connection"),
+      content: Column(
         mainAxisSize: MainAxisSize.min,
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Image.asset('assets/rightEar.png'),
+          Text("Do you hear the sound?"),
         ],
+
       ),
-      actions: <Widget>[
-        new FlatButton(
+      actions: [
+         FlatButton(
             onPressed: () {
-              Navigator.of(context).pop();
+              showDialog(
+                barrierDismissible: false,
+                context: context,
+                builder: (BuildContext context) => _cancelPopUp(context),
+                //should navigate to _cancelPopUp
+              );
+              // Navigator.of(context).pop();
             },
             child: Image.asset('assets/no.png')),
-        new FlatButton(
-          onPressed: () {
-
-          },
+         FlatButton(
+          onPressed: () {},
           child: Image.asset('assets/yes.png'),
         ),
+      ],
+    );
+
+  }
+
+  Widget _cancelPopUp(BuildContext context) {
+    return AlertDialog(
+      title: Text("Check your device connection"),
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            "Your devied needs to be properly \n connected in order to conduct the hearing test",
+          )
+        ],
+      ),
+      actions: [
+        new FlatButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          color: Colors.grey,
+          child: Text("OK"),
+        )
       ],
     );
   }
