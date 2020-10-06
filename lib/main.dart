3ea695f3789e3876f9a0ef74ace844016636c8f8
Anders Brandt <s185016@student.dtu.dk>
2020-10-06 22:31:04 +0200

    removed unused imports in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:hear_better/screens/home.dart';
-import 'package:hear_better/screens/result.dart';
-import 'package:hear_better/screens/test_prep_screens/test_prep_deviceConnection.dart';
-import 'package:hear_better/screens/test_prep_screens/test_prep_environment.dart';
-import 'package:hear_better/screens/test_prep_screens/test_prep_startTestScreen.dart';
-import 'package:hear_better/screens/test_prep_screens/test_prep_volume.dart';
 
 void main() {
   runApp(MyApp());
