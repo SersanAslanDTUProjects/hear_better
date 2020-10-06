e85df7058a7b33b3e1382ee55beb3be7e33cd0eb
Anders Brandt <s185016@student.dtu.dk>
2020-10-06 22:08:03 +0200

    Added logo and background to home page

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -3,14 +3,37 @@ import 'package:flutter/material.dart';
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: Container(
+    return DecoratedBox(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        image: DecorationImage(
+            image: AssetImage(
+              'assets/images/test_prep_images/checking_surrounding.png',
+            ),
+            fit: BoxFit.fitWidth,
+            alignment: Alignment.bottomCenter),
+      ),
       child: Align(
-          alignment: Alignment.center,
-          child: Column(mainAxisSize: MainAxisSize.min, children: [
-            Text("Hello"),
-            FloatingActionButton(onPressed: () {}, child: Icon(Icons.input))
-          ])),
-    ));
+        child: Column(children: [
+          Container(
+            child: Image.asset('assets/images/general_images/logo.png'),
+            margin: EdgeInsets.fromLTRB(30, 50, 0, 250),
+          ),
+          Container(
+            child: RaisedButton(
+              onPressed: () {},
+              padding: const EdgeInsets.all(20),
+              child: Text(
+                "Test your hearing",
+                style: TextStyle(fontSize: 28),
+              ),
+              color: Color.fromRGBO(85, 233, 191, 1),
+              textColor: Colors.white,
+            ),
+            margin: EdgeInsets.fromLTRB(0, 0, 0, 0),
+          ),
+        ]),
+      ),
+    );
   }
 }
