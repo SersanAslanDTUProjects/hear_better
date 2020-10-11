ddd86b9dc9dfe7b7e29869618ec87ef7fb407882
Siff <siff.ravn@gmail.com>
2020-10-11 15:59:06 +0200

    feat(model) create data models

---
+++ lib/models/test_result.dart
@@ -0,0 +1,25 @@
+import 'package:hear_better/models/audiogram.dart';
+
+class TestResult {
+
+  DateTime _timeStamp;
+  Audiogram _audiogram;
+  double _score;
+
+  TestResult(Audiogram audiogram){
+    this._audiogram = audiogram;
+    _timeStamp = new DateTime.now();
+    calcScore(audiogram);
+  }
+
+  void calcScore(Audiogram audiogram) {
+    // TODO impl method to compare audiogram with standard hearing and rate it
+    _score = 0;
+  }
+
+  double get score => _score;
+
+  Audiogram get audiogram => _audiogram;
+
+  DateTime get timeStamp => _timeStamp;
+}
