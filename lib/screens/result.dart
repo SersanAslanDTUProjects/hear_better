4ae0ef21b57100ca0c545ecf329376988cd8c731
magnusbrok <43874955+magnusbrok@users.noreply.github.com>
2020-10-29 14:52:38 +0100

    added dummy data

--- lib/screens/result.dart
+++ lib/screens/result.dart
@@ -1,6 +1,40 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
+import 'package:charts_flutter/flutter.dart' as charts;
+import 'package:hear_better/models/audiogram.dart';
 
 class ResultScreen extends StatelessWidget {
+  List<charts.Series> seriesList = createData();
+
+  Audiogram testGraf = new Audiogram();
+
+
+  static List<charts.Series<TestAudiogram, String>> createData() {
+    final random = Random();
+    final audiogramData = [
+      TestAudiogram(10, random.nextInt(8000)),
+      TestAudiogram(20, random.nextInt(8000)),
+      TestAudiogram(30, random.nextInt(8000)),
+      TestAudiogram(40, random.nextInt(8000)),
+      TestAudiogram(50, random.nextInt(8000)),
+      TestAudiogram(80, random.nextInt(8000)),
+    ];
+
+    return [charts.Series<TestAudiogram, String>(
+      id: 'Audiogram',
+      domainFn: (TestAudiogram testAudiogram, _) => testAudiogram.frequency.toString(),
+      measureFn: (TestAudiogram testAudiogram, _ ) => testAudiogram.decibel,
+      data: audiogramData,
+    )];
+  }
+
+  barChart() {
+    return charts.BarChart(
+      seriesList,
+      animate: true,
+      vertical: true,
+    );
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -50,7 +84,9 @@ class ResultScreen extends StatelessWidget {
             image: AssetImage('assets/images/result-screen/audiogram.png'),
           ),
           Container(
-            child: Image(
+            child:
+            //barChart(),
+            Image(
               image: AssetImage('assets/images/result-screen/Full-Green.png'),
             ),
           ),
@@ -63,4 +99,11 @@ class ResultScreen extends StatelessWidget {
       ),
     );
   }
+}
+
+class TestAudiogram {
+  int decibel;
+  int frequency;
+
+  TestAudiogram(this.decibel, this.frequency);
 }
