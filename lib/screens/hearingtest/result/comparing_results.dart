623a1a49491b1738ddec5be5ef31485a5bc14cef
Helle <helle1926@hotmail.com>
2020-11-08 16:29:55 +0100

    Minor UI changes

--- lib/screens/hearingtest/result/comparing_results.dart
+++ lib/screens/hearingtest/result/comparing_results.dart
@@ -5,6 +5,12 @@ import 'package:hear_better/theme/colors.dart';
 import 'package:percent_indicator/percent_indicator.dart';
 
 TestResult testResult;
+double userTestResult = 30 / 100;
+double normalHearing = 0.5;
+double lowHearing = 0.3;
+double goodHearing = 0.8;
+String resultsOfHearing = "----";
+Color color = AppColors().grey;
 
 class Results extends StatefulWidget {
   @override
@@ -12,13 +18,6 @@ class Results extends StatefulWidget {
 }
 
 class _ResultsState extends State<Results> {
-  double userTestResult = 60 / 100;
-  double normalHearing = 0.5; //what is the interval for a normal hearing
-  double lowHearing = 0.3;
-  double goodHearing = 0.8;
-  String resultsOfHearing = "----";
-  Color color; //get from test_result
-
   String displayTextOfResult(double userTestResult) {
     setState(() {
       if (userTestResult <= lowHearing) {
@@ -45,37 +44,35 @@ class _ResultsState extends State<Results> {
         child: Column(
           children: <Widget>[
             Text(
-              "This is the result of your hearing test: ",
+              "This is the result of your hearing test ",
               style: TextStyle(
                 fontSize: 24,
               ),
+              textAlign: TextAlign.center,
             ),
             Container(
-              padding: EdgeInsets.fromLTRB(0,20,0,20),
+              padding: EdgeInsets.fromLTRB(0, 20, 0, 20),
               child: Center(
                 child: Text(
                   displayTextOfResult(userTestResult),
                   style: TextStyle(
                     fontSize: 18.0,
                   ),
+                  textAlign: TextAlign.center,
                 ),
               ),
             ),
             LinearPercentIndicator(
               animation: true,
-              animationDuration: 1500,
+              animationDuration: 1000,
               lineHeight: 30,
               leading: Padding(
                 padding: EdgeInsets.only(right: 15),
-                child: Icon(
-                  Icons.close
-                ),
+                child: Icon(Icons.close),
               ),
               trailing: Padding(
                 padding: EdgeInsets.only(left: 15),
-                child:  Icon(
-                    Icons.insert_emoticon
-                ),
+                child: Icon(Icons.insert_emoticon),
               ),
               progressColor: color,
               percent: userTestResult,
