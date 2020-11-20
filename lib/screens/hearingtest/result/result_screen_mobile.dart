dd69982fee70c5677cb59f22dbe272faee675a0b
Helle <helle1926@hotmail.com>
2020-11-20 12:02:40 +0100

    some layout details

--- lib/screens/hearingtest/result/result_screen_mobile.dart
+++ lib/screens/hearingtest/result/result_screen_mobile.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart' hide Router;
 import 'package:hear_better/models/audiogram.dart';
 import 'package:hear_better/models/hbuser.dart';
-import 'package:hear_better/screens/hearingtest/result/comparing_Ears.dart';
+import 'package:hear_better/screens/hearingtest/result/comparing_ears.dart';
 import 'package:hear_better/screens/hearingtest/result/comparing_results.dart';
 import 'package:hear_better/theme/app_theme.dart';
 import 'package:hear_better/theme/routes/router.gr.dart';
